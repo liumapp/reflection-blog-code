441519679a65d25cefdcd932ab8ce69e3cf9445d
liumapp <liumapp.com@gmail.com>
2018-07-23 14:55:30 +0800

    add sb bootstrap class

---
+++ src/main/java/com/liumapp/blog/reflection/ReflectionBlogCodeMain.java
@@ -0,0 +1,20 @@
+package com.liumapp.blog.reflection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liumapp
+ * @file ReflectionBlogCodeMain.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/23/18
+ */
+@SpringBootApplication
+public class ReflectionBlogCodeMain {
+
+    public static void main (String[] args) {
+        SpringApplication.run(ReflectionBlogCodeMain.class, args);
+    }
+
+}
